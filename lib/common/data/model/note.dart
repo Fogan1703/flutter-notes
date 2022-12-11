42717d0e3fecb5f45617b6e152a37f7392d0771e
Fogan1703 <blesk.bogdan@gmail.com>
2022-12-11 14:27:02 +0100

    Add note color to model

--- lib/common/data/model/note.dart
+++ lib/common/data/model/note.dart
@@ -1,8 +1,11 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 import '../util/content_parser.dart';
 
 part 'check_item.dart';
+
+part 'note_color.dart';
 
 class NoteFields {
   NoteFields._();
@@ -10,6 +13,7 @@ class NoteFields {
   static const id = 'id';
   static const edited = 'edited';
   static const starred = 'starred';
+  static const color = 'color';
   static const title = 'title';
   static const content = 'content';
 }
@@ -19,23 +23,35 @@ class Note extends Equatable {
     this.id,
     required this.edited,
     required this.starred,
+    required this.color,
     required this.title,
     required this.content,
   });
 
+  factory Note.empty() => Note(
+        edited: DateTime.now(),
+        starred: false,
+        color: null,
+        title: '',
+        content: const [],
+      );
+
   final int? id;
   final DateTime edited;
   final bool starred;
+  final NoteColor? color;
   final String title;
   final List<Object> content;
 
   @override
-  List<Object?> get props => [id, edited, starred, title, content];
+  List<Object?> get props => [id, edited, starred, color, title, content];
 
   factory Note.fromJson(Map<String, Object?> data) => Note(
         id: data[NoteFields.id] as int,
-        edited: DateTime.fromMillisecondsSinceEpoch(data[NoteFields.edited] as int),
+        edited:
+            DateTime.fromMillisecondsSinceEpoch(data[NoteFields.edited] as int),
         starred: data[NoteFields.starred] == 1,
+        color: NoteColor.parse(data[NoteFields.color] as String),
         title: data[NoteFields.title] as String,
         content: ContentParser.parseContent(data[NoteFields.content] as String),
       );
@@ -44,6 +60,7 @@ class Note extends Equatable {
         NoteFields.id: id,
         NoteFields.edited: edited.millisecondsSinceEpoch,
         NoteFields.starred: starred ? 1 : 0,
+        NoteFields.color: color.toString(),
         NoteFields.title: title,
         NoteFields.content: ContentParser.contentToString(content),
       };
---
+++ lib/common/data/model/note_color.dart
@@ -0,0 +1,38 @@
+part of 'note.dart';
+
+class NoteColor extends Equatable {
+  const NoteColor(this.name, this.value);
+
+  final String name;
+  final int value;
+
+  @override
+  List<Object?> get props => [name, value];
+
+  static NoteColor parse(String source) => colors.singleWhere(
+        (color) => color.name == source,
+        orElse: () => throw Exception('Unable to parse color $source'),
+      );
+
+  @override
+  String toString() => name;
+
+  static final colors = [
+    NoteColor('pink', Colors.pink.value),
+    NoteColor('purple', Colors.purple.value),
+    NoteColor('deepPurple', Colors.deepPurple.value),
+    NoteColor('indigo', Colors.indigo.value),
+    NoteColor('blue', Colors.blue.value),
+    NoteColor('lightBlue', Colors.lightBlue.value),
+    NoteColor('cyan', Colors.cyan.value),
+    NoteColor('teal', Colors.teal.value),
+    NoteColor('green', Colors.green.value),
+    NoteColor('lightGreen', Colors.lightGreen.value),
+    NoteColor('lime', Colors.lime.value),
+    NoteColor('yellow', Colors.yellow.value),
+    NoteColor('amber', Colors.amber.value),
+    NoteColor('orange', Colors.orange.value),
+    NoteColor('deepOrange', Colors.deepOrange.value),
+    NoteColor('blueGrey', Colors.blueGrey.value),
+  ];
+}
